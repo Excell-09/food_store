030e5187eb13e6f67f8f68798a067e972e57ab04
excell-09 <excell.agcb12@gmail.com>
2023-07-01 17:54:14 +0700

    register done

---
+++ client/src/components/Input/InputText.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function InputText({ label, type, errorMessage, register }) {
+  return (
+    <div className="my-3 w-full">
+      <label htmlFor={label} className="capitalize">
+        {label}
+      </label>
+      <input
+        className="p-3 block rounded-md w-full bg-slate-50 focus:outline-blue-600"
+        placeholder={"enter your " + label}
+        type={type}
+        id={label}
+        {...register}
+      />
+      {errorMessage && <p className="text-red-500 capitalize">{errorMessage}!</p>}
+    </div>
+  );
+}
--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,7 +1,104 @@
-import React from 'react'
+import Container from "@/components/Container";
+import InputText from "@/components/Input/InputText";
+import { Alert, Button, Checkbox, Divider } from "antd";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import appAxios from "@/utils/AppAxios";
+import { Link } from "react-router-dom";
+
+const schema = yup.object().shape({
+  full_name: yup.string().required("username is required").min(3, "Your username is too short."),
+  email: yup.string().email("email format is not valid!").required("email is required"),
+  password: yup.string().required("password is required").min(6, "Your password is too short."),
+});
 
 export default function Register() {
+  const form = useForm({
+    defaultValues: {
+      full_name: "",
+      email: "",
+      password: "",
+    },
+    resolver: yupResolver(schema),
+  });
+  const { register, handleSubmit, formState } = form;
+  const { errors } = formState;
+
+  const [message, setMessage] = React.useState({ type: "", message: "" });
+  const [loading, setLoading] = React.useState(false);
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleShowPassword = (e) => {
+    setShowPassword(e.target.checked);
+  };
+  const handleLogin = async (data) => {
+    if (loading) setLoading(false);
+    try {
+      const response = await appAxios.post("/auth/register", data);
+      if (response.data.error !== 1) {
+        setMessage({ type: "success", message: "Create Account Success!" });
+      } else {
+        setMessage({ type: "error", message: response.data.massage });
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>Register</div>
-  )
+    <Container>
+      <div className="grid min-h-[80vh]">
+        <form
+          className="m-auto w-full max-w-[500px] bg-white rounded-sm shadow-md p-3"
+          autoComplete="off"
+          onSubmit={handleSubmit(handleLogin)}
+        >
+          <h3 className="text-center text-2xl font-bold">Login</h3>
+          {message.message && (
+            <Alert className="mt-3" message={message.message} type={message.type} showIcon closable />
+          )}
+
+          <Divider />
+
+          <InputText
+            register={register("full_name")}
+            label={"username"}
+            type={"text"}
+            errorMessage={errors.full_name?.message}
+          />
+          <InputText register={register("email")} label={"email"} type={"text"} errorMessage={errors.email?.message} />
+          <InputText
+            register={register("password")}
+            label={"password"}
+            type={showPassword ? "text" : "password"}
+            errorMessage={errors.password?.message}
+          />
+          <div className="block">
+            <Checkbox defaultChecked={false} onClick={handleShowPassword}>
+              Show Password
+            </Checkbox>
+          </div>
+          <Button
+            loading={loading}
+            type="primary"
+            htmlType="submit"
+            className="bg-blue-600 mt-3 me-auto block w-full"
+            size={"large"}
+          >
+            {loading ? "Loading..." : "Register"}
+          </Button>
+          <p className="text-slate-400 text-center mt-3">
+            Already Have an Account?{" "}
+            <Link className="underline text-blue-700 " to={"/login"}>
+              Login
+            </Link>
+          </p>
+        </form>
+      </div>
+    </Container>
+  );
 }
