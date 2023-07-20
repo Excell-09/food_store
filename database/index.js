8b447651a5bb23c7d98045dccce9eeb7445493ec
Jun Choi <103556428+Excell-09@users.noreply.github.com>
2023-07-20 13:20:44 +0700

    Update database

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { dbHost, dbName, dbPass, dbPort, dbUser } = require("../app/config");
 
 mongoose.connect(
-  `mongodb+srv://${dbUser}:${dbPass}@foodstore.4tgn1tv.mongodb.net/${dbName}?retryWrites=true&w=majority`
+  `mongodb+srv://${dbUser}:${dbPass}@foodstore.3kqbupj.mongodb.net/${dbName}?retryWrites=true&w=majority`
 );
 
 const db = mongoose.connection;
