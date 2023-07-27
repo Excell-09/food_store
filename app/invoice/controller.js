c42b89ec20880614227d7306da2ecabbe5328382
excell-09 <excell.agcb12@gmail.com>
2023-07-27 10:34:17 +0700

    add new controller on invoice to fetch all invoice admin site

--- app/invoice/controller.js
+++ app/invoice/controller.js
@@ -1,51 +1,80 @@
-const { subject } = require("@casl/ability");
+const {subject} = require("@casl/ability");
 const Invoice = require("./model");
-const { policyFor } = require("../../utils");
+const {policyFor} = require("../../utils");
 
 const view = async (req, res) => {
-  try {
-    const { order_id } = req.params;
-    let invoice = await Invoice.findOne({ order: order_id }).populate("order").populate("user");
-    let policy = policyFor(req.user);
-    let subjectInvoice = subject("Invoice", { ...invoice, user_id: invoice.user._id });
-    if (!policy.can("read", subjectInvoice)) {
-      return res.json({
-        error: 1,
-        message: "Anda tidak memiliki akses untuk melihat ini!",
-      });
-    }
+    try {
+        const {order_id} = req.params;
+        let invoice = await Invoice.findOne({order: order_id})
+            .populate("order")
+            .populate("user");
+        let policy = policyFor(req.user);
+        let subjectInvoice = subject("Invoice", {
+            ...invoice,
+            user_id: invoice.user._id,
+        });
+        if (!policy.can("read", subjectInvoice)) {
+            return res.json({
+                error: 1,
+                message: "Anda tidak memiliki akses untuk melihat ini!",
+            });
+        }
 
-    return res.json(invoice);
-  } catch (err) {
-    return res.json({
-      error: 1,
-      message: "Error when getting invoice!",
-    });
-  }
+        return res.json(invoice);
+    } catch (err) {
+        console.log({err});
+        return res.json({
+            error: 1,
+            message: "Error when getting invoice!",
+        });
+    }
 };
 
 const index = async (req, res) => {
-  try {
-    let invoices = await Invoice.find({ user: req.user._id })
-      .populate("order")
-      .populate("user")
-      .populate({
-        path: "order",
-        populate: {
-          path: "order_items",
-        },
-      })
-      .sort("-updatedAt");
-    return res.json(invoices);
-  } catch (err) {
-    return res.json({
-      error: 1,
-      message: "Error when getting invoice!",
-    });
-  }
+    try {
+        let invoices = await Invoice.find({user: req.user._id})
+            .populate("order")
+            .populate("user")
+            .populate({
+                path: "order",
+                populate: {
+                    path: "order_items",
+                },
+            })
+            .sort("-updatedAt");
+        return res.json(invoices);
+    } catch (err) {
+        return res.json({
+            error: 1,
+            message: "Error when getting invoice!",
+        });
+    }
+};
+
+const getAllInvoices = async (_, res) => {
+    try {
+        let invoices = await Invoice.find()
+            .populate("order")
+            .populate("user")
+            .populate({
+                path: "order",
+                populate: {
+                    path: "order_items",
+                },
+            })
+            .sort("-updatedAt");
+
+        return res.json(invoices);
+    } catch (err) {
+        return res.json({
+            error: 1,
+            message: "Error when getting invoice!",
+        });
+    }
 };
 
 module.exports = {
-  index,
-  view,
+    index,
+    view,
+    getAllInvoices,
 };
--- app/invoice/router.js
+++ app/invoice/router.js
@@ -1,9 +1,14 @@
 const router = require("express").Router();
 
-const { policy_check } = require("../../middlewares/middleware");
+const {policy_check} = require("../../middlewares/middleware");
 const invoiceController = require("./controller");
 
 router.get("/", policy_check("view", "Invoice"), invoiceController.index);
+router.get(
+    "/invoices",
+    policy_check("view", "Invoice"),
+    invoiceController.getAllInvoices
+);
 router.get("/:order_id", invoiceController.view);
 
 module.exports = router;
